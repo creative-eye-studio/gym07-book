56d9bfaef0083d7f52c36586320fed0357643756
Kévin R <rifakev@gmail.com>
2023-12-24 17:24:36 +0100

    KR - Corrections techniques

--- src/Controller/AdminUsersController.php
+++ src/Controller/AdminUsersController.php
@@ -73,7 +73,7 @@ class AdminUsersController extends AbstractController
     
                 $rolesToExclude = ['ROLE_ANNUEL', 'ROLE_ADMIN', 'ROLE_6MOIS', 'ROLE_3MOIS', 'ROLE_1MOIS', 'ROLE_ETU_SEN', 'ROLE_FONCTIONNAIRE'];
                 if (count(array_intersect($rolesToExclude, $user->getRoles())) === 0) {
-                    if ($user->getFreeCourses() == 0) {
+                    if ($user->getFreeCourses() == 0 && $user->getCredits() > 0) {
                         $user->setCredits($user->getCredits() - $plan->getCours()->getCredits());
                     }
                 }
